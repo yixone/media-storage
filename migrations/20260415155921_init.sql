16c42727a09cc66533fbcca0d5d00a818d5deef5
Александр <sasha-kvasnikov@mail.ru>
2026-04-15 19:07:22 +0300

    add base migration

---
+++ migrations/20260415155921_init.sql
@@ -0,0 +1,25 @@
+CREATE TABLE assets (
+    id          UUID            NOT NULL,
+    media       VARCHAR(64)     NOT NULL,
+    created_at  TIMESTAMPTZ     NOT NULL,
+    caption     VARCHAR(1024),
+    source_url  VARCHAR(512),
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (media) REFERENCES media(id) ON DELETE CASCADE
+);
+
+CREATE TABLE media (
+    id          VARCHAR(64)     NOT NULL,
+    state       VARCHAR(28)     NOT NULL    CHECK (
+        state IN ('pending', 'processing', 'ready', 'failed')
+    ),
+    created_at  TIMESTAMPTZ     NOT NULL,
+    size        BIGINT          NOT NULL,
+    mimetype    VARCHAR(128)    NOT NULL,
+    width       INT,
+    height      INT,
+    color       VARCHAR(6),
+
+    PRIMARY KEY (id)
+);
