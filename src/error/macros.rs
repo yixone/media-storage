898f8f49e76c63bbf683967e29a8766cc59d561f
Александр <sasha-kvasnikov@mail.ru>
2026-04-16 12:41:22 +0300

    remove `AppError.source`

--- src/error/macros.rs
+++ src/error/macros.rs
@@ -3,15 +3,7 @@
 macro_rules! create_error {
     ( $error_type: ident $( { $( $arg_id: ident: $arg_expr: expr ),* } )? ) => {
         $crate::error::AppError::new(
-            $crate::error::ErrorType::$error_type $( { $( $arg_id: $arg_expr ),* } )?,
-            None
-        )
-    };
-
-    ( $error_type: ident $( { $( $arg_id: ident: $arg_expr: expr ),* } )?, $source: expr ) => {
-        $crate::error::AppError::new(
-            $crate::error::ErrorType::$error_type $( { $( $arg_id: $arg_expr ),* } )?,
-            Some(Box::new($source))
+            $crate::error::ErrorType::$error_type $( { $( $arg_id: $arg_expr ),* } )?
         )
     };
 }
@@ -21,8 +13,8 @@ macro_rules! map_error {
     ( $original: path => $error: ident ) => {
         impl From<$original> for $crate::error::AppError {
             #[track_caller]
-            fn from(e: $original) -> Self {
-                $crate::create_error!($error, e)
+            fn from(_: $original) -> Self {
+                $crate::create_error!($error)
             }
         }
     };
--- src/error/mod.rs
+++ src/error/mod.rs
@@ -5,8 +5,6 @@ mod macros;
 
 pub type Result<T> = std::result::Result<T, AppError>;
 
-type ErrorSourceDyn = Box<dyn std::error::Error + Send + Sync + 'static>;
-
 /// Application error types
 #[derive(Debug, PartialEq)]
 pub enum ErrorType {
@@ -20,19 +18,17 @@ pub enum ErrorType {
 pub struct AppError {
     error_type: ErrorType,
     location: String,
-    source: Option<ErrorSourceDyn>,
 }
 
 impl AppError {
     /// Creates a new [`AppError`]
     #[track_caller]
-    pub fn new(error_type: ErrorType, source: Option<ErrorSourceDyn>) -> Self {
+    pub fn new(error_type: ErrorType) -> Self {
         let caller = Location::caller();
         let location = format!("{}:{}", caller.file(), caller.line());
         Self {
             error_type,
             location,
-            source,
         }
     }
 
@@ -48,11 +44,7 @@ impl std::fmt::Display for AppError {
     }
 }
 
-impl std::error::Error for AppError {
-    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-        self.source.as_deref().map(|s| s as &dyn std::error::Error)
-    }
-}
+impl std::error::Error for AppError {}
 
 map_error!(std::io::Error => InternalError);
 map_error!(sqlx::Error => InternalError);
