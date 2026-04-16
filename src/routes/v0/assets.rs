eaafac62c6c48e331940a0c8cc8e9ec766d600b8
Александр <sasha-kvasnikov@mail.ru>
2026-04-16 22:13:56 +0300

    add base `assets` endpoints

--- src/routes/v0/assets.rs
+++ src/routes/v0/assets.rs
@@ -1,6 +1,34 @@
-use actix_web::web;
+use actix_multipart::Multipart;
+use actix_web::{HttpResponse, get, post, web};
+
+use crate::{
+    di::{DataContext, NotificationsContext},
+    error::Result,
+};
 
 /// Configures `/assets` endpoints
 pub fn config(cfg: &mut web::ServiceConfig) {
-    //
+    cfg.service(web::scope("assets").service(upload_asset));
+}
+
+/// Uploads new Asset and returns it
+#[post("")]
+pub async fn upload_asset(
+    mut payload: Multipart,
+    ctx: web::Data<DataContext>,
+    nt: web::Data<NotificationsContext>,
+) -> Result<HttpResponse> {
+    todo!()
+}
+
+/// Returns an Asset by ID
+#[get("/{id}")]
+pub async fn get_asset_by_id(ctx: web::Data<DataContext>) -> Result<HttpResponse> {
+    todo!()
+}
+
+/// Returns a list of Assets with pagination
+#[get("")]
+pub async fn get_assets_list(ctx: web::Data<DataContext>) -> Result<HttpResponse> {
+    todo!()
 }
--- src/routes/v0/mod.rs
+++ src/routes/v0/mod.rs
@@ -5,6 +5,9 @@ pub mod media;
 
 /// Configures `/v0` endpoints
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("assets").configure(assets::config))
-        .service(web::scope("media").configure(media::config));
+    cfg.service(
+        web::scope("v0")
+            .configure(assets::config)
+            .configure(media::config),
+    );
 }
