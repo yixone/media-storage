09175988e9813fe011e74be8556e9b2d5667df4e
Александр <sasha-kvasnikov@mail.ru>
2026-04-20 19:36:36 +0300

    api/core: add base `WorkerHost` methods

--- src/bg/host.rs
+++ src/bg/host.rs
@@ -11,5 +11,56 @@ pub struct WorkersHost {
     pub(super) units: Vec<Arc<dyn WorkerUnit>>,
 
     /// Workers cancellation token
-    pub(super) stop: CancellationToken,
+    pub(super) cancellation: CancellationToken,
+}
+
+impl WorkersHost {
+    /// Creates a new [`WorkersHost`]
+    pub fn new() -> Self {
+        let cancellation = CancellationToken::new();
+        WorkersHost {
+            units: Vec::new(),
+            cancellation,
+        }
+    }
+
+    /// Creates a new [`WorkersHost`] with [`CancellationToken`]
+    pub fn with_cancellation(cancellation: CancellationToken) -> Self {
+        WorkersHost {
+            units: Vec::new(),
+            cancellation,
+        }
+    }
+
+    /// Adds the given [`BaseWorker`] to the [`WorkersHost`]
+    pub fn with_worker(mut self, unit: impl BaseWorker) -> Self {
+        self.units.push(Arc::new(unit));
+        self
+    }
+
+    /// Starts [`WorkersHost`] units
+    pub async fn run_units(&self) {
+        for u in &self.units {
+            let cancellation = self.cancellation.clone();
+            let unit = u.clone();
+
+            tokio::spawn(async move {
+                let rt = unit.run(cancellation).await;
+                if let Err(e) = rt {
+                    tracing::error!(error = ?e, "background_worker.runtime_error");
+                }
+            });
+        }
+    }
+
+    /// Causes all background workers to stop
+    pub async fn stop_units(&self) {
+        self.cancellation.cancel();
+    }
+}
+
+impl Default for WorkersHost {
+    fn default() -> Self {
+        Self::new()
+    }
 }
