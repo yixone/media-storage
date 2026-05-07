87cda727ba8f276f947ee560696d3933304df8d4
Александр <sasha-kvasnikov@mail.ru>
2026-05-07 17:17:22 +0300

    feat: add default for patch mod

--- libs/shared_models/src/patch.rs
+++ libs/shared_models/src/patch.rs
@@ -1,5 +1,7 @@
+#[derive(Debug, Default)]
 pub enum PatchField<T> {
     Update(T),
+    #[default]
     Ignore,
 }
 
@@ -7,6 +9,7 @@ macro_rules! patch_model {
     (
         $model_name: ident { $( $f_id: ident: $f_ty: ty ),* }
     ) => {
+        #[derive(Default)]
         pub struct $model_name {
             $(pub $f_id: $crate::patch::PatchField<$f_ty> ),*
         }
