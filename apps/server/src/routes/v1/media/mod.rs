1de73787a2c18fa4e0d14b802cd59d78aa810b0e
Александр <sasha-kvasnikov@mail.ru>
2026-05-09 16:26:33 +0300

    fix: fix missing media route

--- apps/server/src/routes/v1/media/mod.rs
+++ apps/server/src/routes/v1/media/mod.rs
@@ -3,5 +3,5 @@ use actix_web::web;
 mod get_stream;
 
 pub fn config(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("media"));
+    cfg.service(web::scope("media").service(get_stream::get_media_stream));
 }
