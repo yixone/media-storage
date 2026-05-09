f7aefd0d83e8ed704e5580f6acc2a938a0199690
Александр <sasha-kvasnikov@mail.ru>
2026-05-09 13:12:51 +0300

    feat: add server api models

--- apps/server/src/lib.rs
+++ apps/server/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod di;
+pub mod error;
 pub mod http;
 pub mod models;
 pub mod routes;
---
+++ apps/server/src/models/v1/media.rs
@@ -0,0 +1,35 @@
+use chrono::{DateTime, Utc};
+use ms_shared_models::domains::{Media, MediaId, MediaStatus};
+use serde::Serialize;
+
+#[derive(Debug, Serialize)]
+pub struct ApiMedia {
+    pub id: MediaId,
+
+    pub created_at: DateTime<Utc>,
+
+    pub blob_size: i64,
+    pub content_type: String,
+
+    pub color: Option<String>,
+
+    pub width: Option<u16>,
+    pub height: Option<u16>,
+
+    pub status: MediaStatus,
+}
+
+impl From<Media> for ApiMedia {
+    fn from(m: Media) -> Self {
+        ApiMedia {
+            id: m.id,
+            created_at: m.created_at,
+            blob_size: m.blob_size,
+            content_type: m.content_type,
+            color: m.color,
+            width: m.width,
+            height: m.height,
+            status: m.status,
+        }
+    }
+}
