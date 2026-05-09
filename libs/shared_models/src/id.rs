503d56f1652112d3bc84c470cbef982066e3d18e
Александр <sasha-kvasnikov@mail.ru>
2026-05-09 13:15:06 +0300

    improve id generator

--- libs/shared_models/src/id.rs
+++ libs/shared_models/src/id.rs
@@ -15,9 +15,8 @@ pub fn generate_i64_id() -> i64 {
     const TIMESTAMP_EPOCH: i64 = 1735678800000;
 
     let timestamp = (chrono::Utc::now().timestamp_millis() - TIMESTAMP_EPOCH) as u64;
+    let rand = rand::random::<u16>() as u64;
 
-    let rand = rand::random::<u32>() as u64;
-
-    let id_u64 = (timestamp << 16) | (rand & 0xFFFF);
-    id_u64 as i64
+    // [ TIMESTAMP (48) ] | [ RANDOM (16) ]
+    ((timestamp << 16) | (rand & 0xFFFF)) as i64
 }
